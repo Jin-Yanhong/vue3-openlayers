32b90dbf4b4405f98b21c60d4b5a53ffd9a6153e
Jin-Yanhong <820877998@qq.com>
2023-02-08 21:11:22 +0800

    add prettier config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+	publicPath: process.env.NODE_ENV === "production" ? "/vue3-openlayers" : "/",
+	lintOnSave: process.env.NODE_ENV === "development",
 	transpileDependencies: true,
+	productionSourceMap: false,
 	devServer: {
-		port: 8000,
-		host: "localhost",
+		port: 5000,
 		open: true,
+		host: "localhost",
+		proxy: {
+			[process.env.VUE_APP_BASE_API]: {
+				target: process.env.VUE_APP_SERVER_URL,
+				changeOrigin: true, // needed for virtual hosted sites
+				ws: true, // proxy websockets
+				pathRewrite: {
+					["^" + process.env.VUE_APP_BASE_API]: "",
+				},
+			},
+		},
 	},
 });
