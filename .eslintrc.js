9c007e06b1083537105a6bb6316aa979d4b0fd60
Jin-Yanhong <820877998@qq.com>
2023-02-08 20:54:44 +0800

    lint code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/typescript/recommended",
+		"plugin:prettier/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"prettier/prettier": [2, { useTabs: true }],
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)",
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
